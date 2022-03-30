a66349d8ff65fdec7cf357302b483a43501f1ed7
Pranav Pavithran Sheena <1288@trenser.com>
2022-03-30 21:06:32 +0530

    reset

--- mind-ar-js-master/src/image-target/aframe.js
+++ mind-ar-js-master/src/image-target/aframe.js
@@ -99,75 +99,77 @@ AFRAME.registerSystem('mindar-image-system', {
 
     // navigator.mediaDevices.getUserMedia({
     //   audio: false, video: {
-    //     facingMode: (this.shouldFaceUser? 'face': 'environment')
+    //     facingMode: (this.shouldFaceUser ? 'face' : 'environment')
     //   }
     // }).then((stream) => {
     //   this.video.addEventListener('loadedmetadata', () => {
-    //     //console.log("video ready...", this.video);
     //     this.video.setAttribute('width', this.video.videoWidth);
     //     this.video.setAttribute('height', this.video.videoHeight);
     //     this._startAR();
     //   });
     //   this.video.srcObject = stream;
-
     // }).catch((err) => {
     //   console.log("getUserMedia error", err);
     //   this.el.emit("arError", { error: 'VIDEO_FAIL' });
     // });
 
-    //Test browser support
-    const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+    navigator.mediaDevices.enumerateDevices().then(devices => {
 
-    if (SUPPORTS_MEDIA_DEVICES) {
-      //Get the environment camera (usually the second one)
-      navigator.mediaDevices.enumerateDevices().then(devices => {
+      const cameras = devices.filter((device) => device.kind === 'videoinput');
 
-        const cameras = devices.filter((device) => device.kind === 'videoinput');
+      if (cameras.length === 0) {
+        throw 'No camera found on this device.';
+      }
+      const camera = cameras[cameras.length - 1];
 
-        if (cameras.length === 0) {
-          throw 'No camera found on this device.';
-        }
-        const camera = cameras[cameras.length - 1];
-
-        // Create stream and get video track
-        navigator.mediaDevices.getUserMedia({
-          video: {
-            // deviceId: camera.deviceId,
-            facingMode:  'environment',
-            // height: {ideal: 1080},
-            // width: {ideal: 1920}
+      // Create stream and get video track
+      navigator.mediaDevices.getUserMedia({
+        video: {
+          deviceId: camera.deviceId,
+          audio: false, video: {
+            facingMode: (this.shouldFaceUser ? 'face' : 'environment')
           }
-        }).then(stream => {
-          const track = stream.getVideoTracks()[0];
+        }
+      }).then(stream => {
 
-          //Create image capture object and get camera capabilities
-          const imageCapture = new ImageCapture(track)
-          const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+        this.video.addEventListener('loadedmetadata', () => {
+          this.video.setAttribute('width', this.video.videoWidth);
+          this.video.setAttribute('height', this.video.videoHeight);
+          this._startAR();
+        });
+        this.video.srcObject = stream;
 
-            //todo: check if camera has a torch
+        const track = stream.getVideoTracks()[0];
+        //Create image capture object and get camera capabilities
+        const imageCapture = new ImageCapture(track)
+        const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+        
+          // track.applyConstraints({
+          //   advanced: [{ torch: this.flashLight }]
+          // });
 
-            //let there be light!
-            const btn = document.querySelector('#flash_on_switch');
-            btn.addEventListener('click', function () {
-              track.applyConstraints({
-                advanced: [{ torch: true }]
-              });
+
+          const onBtn = document.querySelector('#flash_on_switch');
+          const offBtn = document.querySelector('#flash_off_switch');
+
+          onBtn.addEventListener('click', function () {
+            track.applyConstraints({
+              advanced: [{ torch: true}]
             });
-            this.video.srcObject = stream;
-            this.video.addEventListener('loadedmetadata', () => {
-              //console.log("video ready...", this.video);
-              this.video.setAttribute('width', this.video.videoWidth);
-              this.video.setAttribute('height', this.video.videoHeight);
-              this._startAR();
-            })
           });
+
+          offBtn.addEventListener('click', function () {
+            track.applyConstraints({
+              advanced: [{ torch: false}]
+            });
+          });
+
+
+
         });
       });
+    });
 
-      //The light will be on as long the track exists
-
-
-    }
 
   },
 
