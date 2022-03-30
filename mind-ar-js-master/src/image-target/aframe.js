75f4cc9287ff767331ce0be845d1439aa960d358
Pranav Pavithran Sheena <1288@trenser.com>
2022-03-30 20:52:48 +0530

    update flas

--- mind-ar-js-master/src/image-target/aframe.js
+++ mind-ar-js-master/src/image-target/aframe.js
@@ -97,24 +97,77 @@ AFRAME.registerSystem('mindar-image-system', {
       return;
     }
 
-    navigator.mediaDevices.getUserMedia({
-      audio: false, video: {
-        facingMode: (this.shouldFaceUser? 'face': 'environment')
-      }
-    }).then((stream) => {
-      this.video.addEventListener('loadedmetadata', () => {
-        //console.log("video ready...", this.video);
-        this.video.setAttribute('width', this.video.videoWidth);
-        this.video.setAttribute('height', this.video.videoHeight);
-        this._startAR();
-      });
-      this.video.srcObject = stream;
-      
-    }).catch((err) => {
-      console.log("getUserMedia error", err);
-      this.el.emit("arError", { error: 'VIDEO_FAIL' });
-    });
+    // navigator.mediaDevices.getUserMedia({
+    //   audio: false, video: {
+    //     facingMode: (this.shouldFaceUser? 'face': 'environment')
+    //   }
+    // }).then((stream) => {
+    //   this.video.addEventListener('loadedmetadata', () => {
+    //     //console.log("video ready...", this.video);
+    //     this.video.setAttribute('width', this.video.videoWidth);
+    //     this.video.setAttribute('height', this.video.videoHeight);
+    //     this._startAR();
+    //   });
+    //   this.video.srcObject = stream;
 
+    // }).catch((err) => {
+    //   console.log("getUserMedia error", err);
+    //   this.el.emit("arError", { error: 'VIDEO_FAIL' });
+    // });
+
+    //Test browser support
+    const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+
+    if (SUPPORTS_MEDIA_DEVICES) {
+      //Get the environment camera (usually the second one)
+      navigator.mediaDevices.enumerateDevices().then(devices => {
+
+        const cameras = devices.filter((device) => device.kind === 'videoinput');
+
+        if (cameras.length === 0) {
+          throw 'No camera found on this device.';
+        }
+        const camera = cameras[cameras.length - 1];
+
+        // Create stream and get video track
+        navigator.mediaDevices.getUserMedia({
+          video: {
+            // deviceId: camera.deviceId,
+            facingMode: ['user', 'environment'],
+            // height: {ideal: 1080},
+            // width: {ideal: 1920}
+          }
+        }).then(stream => {
+          const track = stream.getVideoTracks()[0];
+
+          //Create image capture object and get camera capabilities
+          const imageCapture = new ImageCapture(track)
+          const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+
+            //todo: check if camera has a torch
+
+            //let there be light!
+            const btn = document.querySelector('#flash_on_switch');
+            btn.addEventListener('click', function () {
+              track.applyConstraints({
+                advanced: [{ torch: true }]
+              });
+            });
+            this.video.srcObject = stream;
+            this.video.addEventListener('loadedmetadata', () => {
+              //console.log("video ready...", this.video);
+              this.video.setAttribute('width', this.video.videoWidth);
+              this.video.setAttribute('height', this.video.videoHeight);
+              this._startAR();
+            })
+          });
+        });
+      });
+
+      //The light will be on as long the track exists
+
+
+    }
 
   },
 
