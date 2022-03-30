05749e6d4c62491921d7bfa914f8269258199692
Pranav Pavithran Sheena <1288@trenser.com>
2022-03-30 08:51:06 +0530

    flash commented

--- mind-ar-js-master/src/image-target/aframe.js
+++ mind-ar-js-master/src/image-target/aframe.js
@@ -97,78 +97,73 @@ AFRAME.registerSystem('mindar-image-system', {
       return;
     }
 
-    // navigator.mediaDevices.getUserMedia({
-    //   audio: false, video: {
-    //     facingMode: (this.shouldFaceUser ? 'face' : 'environment')
-    //   }
-    // }).then((stream) => {
-    //   this.video.addEventListener('loadedmetadata', () => {
-    //     this.video.setAttribute('width', this.video.videoWidth);
-    //     this.video.setAttribute('height', this.video.videoHeight);
-    //     this._startAR();
-    //   });
-    //   this.video.srcObject = stream;
-    // }).catch((err) => {
-    //   console.log("getUserMedia error", err);
-    //   this.el.emit("arError", { error: 'VIDEO_FAIL' });
-    // });
-
-    navigator.mediaDevices.enumerateDevices().then(devices => {
-
-      const cameras = devices.filter((device) => device.kind === 'videoinput');
-
-      if (cameras.length === 0) {
-        throw 'No camera found on this device.';
+    navigator.mediaDevices.getUserMedia({
+      audio: false, video: {
+        facingMode: (this.shouldFaceUser ? 'face' : 'environment')
       }
-      const camera = cameras[cameras.length - 1];
-
-      // Create stream and get video track
-      navigator.mediaDevices.getUserMedia({
-        video: {
-          deviceId: camera.deviceId,
-          audio: false, video: {
-            facingMode: (this.shouldFaceUser ? 'face' : 'environment')
-          }
-        }
-      }).then(stream => {
-
-        this.video.addEventListener('loadedmetadata', () => {
-          this.video.setAttribute('width', this.video.videoWidth);
-          this.video.setAttribute('height', this.video.videoHeight);
-          this._startAR();
-        });
-        this.video.srcObject = stream;
-
-        const track = stream.getVideoTracks()[0];
-        //Create image capture object and get camera capabilities
-        const imageCapture = new ImageCapture(track)
-        const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
-        
-          // track.applyConstraints({
-          //   advanced: [{ torch: this.flashLight }]
-          // });
-
-
-          const onBtn = document.querySelector('#flash_on_switch');
-          const offBtn = document.querySelector('#flash_off_switch');
-
-          onBtn.addEventListener('click', function () {
-            track.applyConstraints({
-              advanced: [{ torch: true}]
-            });
-          });
-
-          offBtn.addEventListener('click', function () {
-            track.applyConstraints({
-              advanced: [{ torch: false}]
-            });
-          });
-
-
-
-        });
+    }).then((stream) => {
+      this.video.addEventListener('loadedmetadata', () => {
+        this.video.setAttribute('width', this.video.videoWidth);
+        this.video.setAttribute('height', this.video.videoHeight);
+        this._startAR();
       });
+      this.video.srcObject = stream;
+    }).catch((err) => {
+      console.log("getUserMedia error", err);
+      this.el.emit("arError", { error: 'VIDEO_FAIL' });
     });
+
+    // navigator.mediaDevices.enumerateDevices().then(devices => {
+
+    //   const cameras = devices.filter((device) => device.kind === 'videoinput');
+
+    //   if (cameras.length === 0) {
+    //     throw 'No camera found on this device.';
+    //   }
+    //   const camera = cameras[cameras.length - 1];
+
+    //   // Create stream and get video track
+    //   navigator.mediaDevices.getUserMedia({
+    //     video: {
+    //       deviceId: camera.deviceId,
+    //       audio: false, video: {
+    //         facingMode: (this.shouldFaceUser ? 'face' : 'environment')
+    //       }
+    //     }
+    //   }).then(stream => {
+
+    //     this.video.addEventListener('loadedmetadata', () => {
+    //       this.video.setAttribute('width', this.video.videoWidth);
+    //       this.video.setAttribute('height', this.video.videoHeight);
+    //       this._startAR();
+    //     });
+    //     this.video.srcObject = stream;
+
+    //     const track = stream.getVideoTracks()[0];
+    //     //Create image capture object and get camera capabilities
+    //     const imageCapture = new ImageCapture(track)
+    //     const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+        
+    //       const onBtn = document.querySelector('#flash_on_switch');
+    //       const offBtn = document.querySelector('#flash_off_switch');
+
+    //       onBtn.addEventListener('click', function () {
+    //         track.applyConstraints({
+    //           advanced: [{ torch: true}]
+    //         });
+    //       });
+
+    //       offBtn.addEventListener('click', function () {
+    //         track.applyConstraints({
+    //           advanced: [{ torch: false}]
+    //         });
+    //       });
+
+
+
+    //     });
+    //   });
+    // });
 
 
   },
